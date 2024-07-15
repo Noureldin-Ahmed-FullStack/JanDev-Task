4914575c70150b1816a3f7833054b4edc053630a
Noureldin <noureldin2662002@gmail.com>
2024-07-15 16:01:29 +0300

    getting ready for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 // import App from './App.tsx'
 import './index.css'
 import './App.css'
-import Products from './Components/Products.tsx'
-import Cart from './Components/Cart.tsx';
 import List from './Components/List.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
